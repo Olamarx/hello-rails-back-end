3a70070fd15f2e4336414697497624bb2e4b402d
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-10-19 07:15:25 +0100

    Add all files to make the app work

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
-    get 'greetings/index'
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
   end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
